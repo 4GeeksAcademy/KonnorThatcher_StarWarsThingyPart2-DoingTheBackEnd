266449d11faf7c8bf1d554cbbcb3305666968474
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-08-08 01:01:38 +0000

    Added tables to models.py

--- src/api/models.py
+++ src/api/models.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +17,79 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Character(db.Model):
+    __tablename__ = 'character'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    gender = db.Column(db.String(10))
+    birth_year = db.Column(db.String(20))
+    species = db.Column(db.String(100))
+    eye_color = db.Column(db.String(20))
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    homeworld = db.Column(db.String(200))
+    image_url = db.Column(db.String(200))
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Character {self.name}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "species": self.species,
+            "eye_color": self.eye_color,
+            "height": self.height,
+            "weight": self.weight,
+            "homeworld": self.weight,
+            "image_url": self.image_url
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+    climate = db.Column(db.String(100))
+    diameter = db.Column(db.Integer)
+    hours_in_day = db.Column(db.Integer)
+    days_in_year = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    image_url = db.Column(db.String(200))
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    
+    def __repr__(self):
+        return f'<Planet {self.name}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            "diameter": self.diameter,
+            "hours_in_day": self.hours_in_day,
+            "days_in_year": self.days_in_year,
+            "population": self.population,
+            "image_url": self.image_url
+        }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
